89f1e5391c9d3d38233485eae11d4aec1239d883
Robert Alestra <bobbyalestra135@gmail.com>
2020-10-16 17:53:44 -0400

    cs50

--- scores.c
+++ scores.c
@@ -6,9 +6,9 @@
 int main (void)
 {
     int scores[3];
-    scores[1] =  72 ;
-    scores[2] = 73;
-    scores[3] = 33;
+    scores[0] =  72 ;
+    scores[1] = 73;
+    scores[2] = 33;
 
 printf("Average: %i\n", (score1 + score2 + score3) / 3);
 }
