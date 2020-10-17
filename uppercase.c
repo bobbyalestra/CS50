3dae175475f82a0c2c762a3abc87d8927f970002
Robert Alestra <bobbyalestra135@gmail.com>
2020-10-17 18:28:07 -0400

    cs50

--- uppercase.c
+++ uppercase.c
@@ -3,22 +3,36 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
-
+#include 
 
 int main (void) 
+// {
+//     string s = get_string("Before: ");
+//     printf("After: ");
+//     for (int i = 0; n = strlen(s); i < n; i++)
+//     {
+//         if (s[i] >= 'a' && s[i] <= 'z' )
+//         {
+
+//             printf("%c", s[i] - 32)
+//         }
+//         else
+//         {
+//             printf("%c", s[i]);
+//         }
+//     }
+//     printf("\n");
+// }
+
+// this is the simpler version below
+
 {
     string s = get_string("Before: ");
     printf("After: ");
     for (int i = 0; n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z' )
-        {
-
-            printf("%c", s[i] - 32)
+            printf("%c", toupper(s[i]));
         }
-        else
-        {
-            printf("%c", s[i]");
-        }
-    }
+    
+    printf("\n");
 }
