ff3f950f8a0669b112e7af57b617e1442352e5bc
Robert Alestra <bobbyalestra135@gmail.com>
2020-10-20 10:43:02 -0400

    creating custom data type in c

--- phonebook.c
+++ phonebook.c
@@ -4,11 +4,40 @@
 #include <string.h>
 
 
+
+// a container for multiple data types
+
+typedef struct 
+{
+        string name;
+        phone number;
+
+    /* data */
+};
+person;
+
+
 int main (void)
 
 {
-    string names[4] = {"Emma", "Rodrigo", "Joe", "Bobby"};
-    string numbers[4] = {"617-555-0100", "617-555-0101", "617-555-0102", "617-555-0103"};
+
+    person people[4];
+
+    people[0].name = "Emma";
+    people[0].number = "617-555-0100";
+    
+    people[1].name = "Rodrigo"
+    people[1].number = "617-555-0101"
+
+    people[2].name = "Joe";
+    people[2].number = "617-555-0102";
+
+    people[3].name = "Bobby"
+    people[3].number = "617-555-0103"
+
+    
+    // string names[4] = {"Emma", "Rodrigo", "Joe", "Bobby"};
+    // string numbers[4] = {"617-555-0100", "617-555-0101", "617-555-0102", "617-555-0103"};
 
     for (int i = 0; i < 4; i++) 
     {
