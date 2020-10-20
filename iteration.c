fad0fcb4d7af72468b33a32b9dfa4b520877b6fc
Robert Alestra <bobbyalestra135@gmail.com>
2020-10-20 14:07:43 -0400

    using mario brick example to request how many bricks the pyramid is

---
+++ iteration.c
@@ -0,0 +1,27 @@
+
+
+
+#include. <stdio.h>
+#include <cs50.h>
+
+
+oid draw(int h);
+
+int main (void)
+{
+    int height = get_int("Height: ");
+
+    draw(height)
+}
+
+void draw(int h)
+{
+    for (int i = 1; i <= h; i++)
+    {
+     for  (int j = 1; j <= i; j++)
+     {
+         printf("#");
+     }
+     printf("\n");
+    }
+}
